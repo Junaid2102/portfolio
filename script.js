e07ed9b5821193c78d406a157be209e28c18bd4f
Junaid Alam <102413407+Junaid2102@users.noreply.github.com>
2024-06-23 15:52:32 +0500

    Updated portfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Python Developer", "Odoo Developer"],
+        strings: ["Software Engineer", "Programmer", "Python Developer", "Odoo Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Python Developer", "Odoo Developer"],
+        strings: ["Software Engineer","Programmer", "Python Developer", "Odoo Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
